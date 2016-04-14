4840fc8bfe488dad7c040c847be8fa6ef056cb5d
Kolier <kolier.li@gmail.com>
2016-04-15 00:48:51 +0800

    # Add sample arrays.

---
+++ script/array/sample.php
@@ -0,0 +1,61 @@
+<?php
+// Sample arrays for other scripts.
+// @author Kolier.Li
+
+// =============================================================================
+// Variables
+// =============================================================================
+
+// Integer indexed
+$array_int_number_asc = [ 1, 3, 5, 7, 9 ];
+$array_int_number_desc = [ 9, 8, 5, 3, 1 ];
+$array_int_number_mixed = [ 1, 6, 3, 7, 4 ];
+$array_int_str_asc = [ 'ab', 'bc', 'ce', 'em', 'ml' ];
+$array_int_str_desc = [ 'ab', 'bc', 'ce', 'em', 'ml' ];
+
+// Associated
+$array_assoc_number_asc_asc = [
+  'a' => 1,
+  'b' => 2,
+  'c' => 3,
+  'd' => 4,
+  'e' => 5,
+];
+
+$array_assoc_number_asc_desc = [
+  'a' => 5,
+  'b' => 4,
+  'c' => 3,
+  'd' => 2,
+  'e' => 1,
+];
+
+$array_assoc_number_desc_asc = [
+  'e' => 1,
+  'd' => 2,
+  'c' => 3,
+  'b' => 4,
+  'a' => 5,
+];
+
+$array_assoc_number_desc_desc = [
+  'e' => 5,
+  'd' => 4,
+  'c' => 3,
+  'b' => 2,
+  'a' => 1,
+];
+
+// Mixed
+
+
+
+// =============================================================================
+// Functions
+// =============================================================================
+
+class ArraySample {
+
+
+
+}
